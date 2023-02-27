c3b7297152159937b86924629bbf042d718a97b7
Thales Bezerra <tob@poli.br>
2023-02-27 16:34:48 -0300

    Setup of tab bar and tab navigation

--- App.js
+++ App.js
@@ -2,19 +2,20 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#1C1C1C' }}>
+      <Text style = {{ color: 'white'}}>Home!</Text>
     </View>
   );
 }
 
-function SettingsScreen() {
+function FavouritesScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#1C1C1C'}}>
+      <Text style = {{ color: 'white'}}>Favourites!</Text>
     </View>
   );
 }
@@ -24,9 +25,28 @@ const Tab = createMaterialBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator
+        initialRouteName="Home"
+        activeColor="black"
+        inactiveColor="white"
+        labeled={false}
+        barStyle={{ backgroundColor: '#455A64' }}>
+        <Tab.Screen name="Home" component={HomeScreen} 
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen name="Favourites" component={FavouritesScreen} 
+            options={{
+              tabBarLabel: 'Favourites',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="star" color={color} size={26} />
+              ),
+            }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
