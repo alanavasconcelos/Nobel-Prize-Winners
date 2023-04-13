5b34d6c85351f7e9f86844765c12e71a6311335d
Thales Bezerra <thalesoliveira.bezerra@gmail.com>
2023-04-13 20:37:47 -0300

    Pesquisa agora também encontra laureados.

--- src/functions/search.js
+++ src/functions/search.js
@@ -8,8 +8,17 @@ const searchPrizes = (query) =>
         - stringSimilarity.compareTwoStrings(stringifyPrize(a), query)
     ).slice(0, 10);
 
-const stringifyPrize = ({ awardYear, category, laureates }) =>
-    //`${awardYear} ${category} ${laureates.map(laur => laur.knownName.en).join(", ")}`
-    `${awardYear} ${category.en}`
+const stringifyPrize = ({ awardYear, category, laureates }) => {
+    let laureatesString = ""
+    try {
+        laureatesString = laureates.map(laur => laur.knownName.en).join(", ")
+        return `${awardYear} ${category.en} ${laureatesString}`
+    } catch (error) {
+        console.log("Error in laureate: " + error)
+    }
+    return `${awardYear} ${category.en}`
+}
+
+    
 
 export { searchPrizes }
