2b4104e3a22592bd1b99dd5430f672d8882ca112
Thales Bezerra <thalesoliveira.bezerra@gmail.com>
2023-04-13 20:32:14 -0300

    Resolvido bug da tela piscando branco em transições

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,9 +1,9 @@
 import { View, Text, ImageBackground, ScrollView } from 'react-native';
-import { styles } from '../../styles';
+import { styles, colors } from '../../styles';
 
 export default function Background({ children, title }) {
     return (
-        <View style={styles.container}>
+        <View style={[styles.container, {backgroundColor: colors.primaryDark}]}>
             <ImageBackground source={require('../../assets/home.png')} style={styles.imageBackground} resizeMode='repeat'>
                 <ScrollView style={{ flex: 1, width: '100%' }} contentContainerStyle={{ flexGrow: 1 }}>
                     <Text style={styles.title}>{title}</Text>
